8c16461a2adaecdbc646110538e111161c749f84
REVANTHSAI <revanthsai361@gmail.com>
2020-01-08 15:39:59 +0530

    Added Heap Notes

---
+++ Medium/MinHeap.py
@@ -0,0 +1,22 @@
+"""
+Min-Heap
+1. A min-heap is a binary tree such that the data contained in each node is less
+than(or equal to) the data in that node’s children.
+2. the binary tree is complete
+3. Root node is the min element in the tree
+
+Max-Heap
+1. A max-heap is a binary tree such that the data contained in each node
+is greater than (or equal to) the data in that node’s children.
+2. the binary tree is complete
+3. Root node is the max element in the tree
+
+Heap can be represented as an array -
+
+
+Note -
+- A full binary tree (sometimes proper binary tree or 2-tree) is a tree in which
+  every node other than the leaves has two children.
+- A complete binary tree is a binary tree in which every level, except possibly
+  the last, is completely filled, and all nodes are as far left as possible.
+"""
